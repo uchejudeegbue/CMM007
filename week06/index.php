7d9f5e365c1aa9fd2eaed45817acc556114a790c
1418349 <a@a.com>
2016-03-07 10:31:12 +0000

    0

--- week06/index.php
+++ week06/index.php
@@ -7,8 +7,12 @@
  */
 
 echo "hello world <br>";
-echo "Hello," . " " . "world" . "!"<br>;
-$myname = "Uche jude Egbue"<br>;
+echo "Hello," . " " . "world" . "!" < br>;
+$myname = "Uche jude Egbue" < br>;
 $an = "adult<br>";
 echo "My name is" . "$myname" . "I am an" . "$an";
+$name = "Uju";
+if (name == Uju){
+    echo "I know you";
+}
 ?>
