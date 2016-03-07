aa1899b84868e3082b772621256498ece4a6ce40
uchejudeegbue <lordadmiralty@yahoo.com>
2016-03-08 00:39:25 +0100

    week06

--- week06/MyPhp.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: UCHE EGBUE BENDAC
- * Date: 3/8/2016
- * Time: 12:01 AM
- */
-echo "Uche";
-?>
--- week06/index.php
+++ week06/index.php
@@ -12,23 +12,35 @@ $myname = "Uche jude Egbue<br>";
 $an = "adult<br>";
 echo "My name is " . "$myname" . "I am an " . "$an<br>";
 $name = "Uju<br>";
-if ($name == Uju){
+if ($name == Uju) {
     echo "I know you<br>";
-}
-else {
+} else {
     echo "who are you<br>";
 }
 echo "Aberdeen is the home of specs, mugs and suggage rolls.<br>";
 $myage = "16<br>";
 
-if ($myage == 16){
+if ($myage == 16) {
     echo "Buy Specs<br>";
-}
-else if (($myage > 18)&&($myage <= 21)) {
+} else if (($myage > 18) && ($myage <= 21)) {
     echo "Buy Mugs<br>";
-}
-else if ($myage > 21){
+} else if ($myage > 21) {
     echo "Buy Suggage Rolls";
+}
+$numberOfHobbitts = "2<br>";
+
+switch ($noOfHobbits) {
+    case 1:
+        echo "1 sad hobbit";
+        break;
+    case 2:
+        echo "2 happy hobbits";
+        break;
+    case 3:
+        echo "3 hobbits are a crowd";
+        break;
+    default:
+        echo "all hobbits have gone home";
 }
 
 ?>
