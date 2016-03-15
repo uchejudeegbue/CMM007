28ff73eec61a8141f906f40d30af26af7232414d
1418349 <a@a.com>
2016-03-15 09:53:40 +0000

    week05

--- week05/login.php
+++ week05/login.php
@@ -10,8 +10,11 @@ if (empty ($_POST["username"])|| empty ($_POST["password"]))
 {
     echo "Both fields are required.";
 }
-$username=$_POST['username'];
-$password=$_POST['password'];
+else{
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+}
+
 
 $sql="SELECT uid FROM users WHERE username='$username' and password='password'";
 
